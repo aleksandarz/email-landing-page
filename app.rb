eec93c20ab798e3dc013da24c7ab986ae45c5e9b
Aleksandar <mail@aleksandarzahariev.com>
2013-01-20 15:06:56 +0200

    fname

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ post '/signup' do
     mailchimp = Hominid::API.new(settings.mailchimp_api_key)
     list_id = mailchimp.find_list_id_by_name(settings.mailchimp_list_name)
     raise "Unable to retrieve list id from MailChimp API." unless list_id
-    mailchimp.list_subscribe(list_id, email, {'FNAME' => 'Joe', 'LNAME' => 'Public'}, 'html', false, true, true, false)
+    mailchimp.list_subscribe(list_id, email, {'FNAME','LNAME'}, 'html', false, true, true, false)
   end
   "Success."
 end
